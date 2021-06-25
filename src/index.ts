a1899fde91d45628b6a9345dae6f023c1e950467
Álvaro Veiga <alvaromrveiga@gmail.com>
2021-06-25 16:50:27 -0300

    Listen on port from .env

--- src/index.ts
+++ src/index.ts
@@ -18,6 +18,6 @@ app.use(
   }
 );
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(process.env.PORT, () => {
+  console.log("Server is running on port " + process.env.PORT);
 });
