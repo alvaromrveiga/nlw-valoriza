bc13bbb49cdcaca055d0a046873b030f3c1c71c5
Álvaro Veiga <alvaromrveiga@gmail.com>
2021-06-25 22:50:59 -0300

    Join db info in DB_URL

--- ormconfig.js
+++ ormconfig.js
@@ -1,10 +1,8 @@
+console.log(process.env.DB_URL);
+
 module.exports = {
   type: "postgres",
-  database: process.env.DB_DATABASE,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
+  url: process.env.DB_URL,
   migrations: ["src/database/migrations/**.ts"],
   entities: ["src/entities/**.ts"],
   cli: {
