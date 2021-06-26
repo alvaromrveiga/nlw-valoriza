6c5dfc6d00fafe89095094a9d7c8145a1286adcb
Álvaro Veiga <alvaromrveiga@gmail.com>
2021-06-26 15:07:30 -0300

    Separete server and client

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,11 +5,14 @@ import "./database";
 import { router } from "./routes";
 import { KnownError } from "./errors/KnownError";
 import cors from "cors";
+import path from "path";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(router);
+
+app.use(express.static(path.join(__dirname, "../../client/build")));
 
 app.use(
   (error: Error, request: Request, response: Response, next: NextFunction) => {
@@ -20,6 +23,12 @@ app.use(
   }
 );
 
-app.listen(process.env.PORT, () => {
-  console.log("Server is running on port " + process.env.PORT);
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../client/build/index.html"));
+});
+
+const port = process.env.PORT || 3333;
+
+app.listen(port, () => {
+  console.log("Server is running on port " + port);
 });
